f4d1d12f62bdeb154b306b93ee10372261c12903
PC <rafalpawezowski@gmail.com>
2025-01-11 21:51:14 +0100

    file name updated in line with naming convention plus print statements removal

---
+++ word_counter/kata_word_counter1.py
@@ -0,0 +1,12 @@
+
+
+def count_words(sentence, delimiter):
+    my_list = sentence.split(delimiter)
+    counter = 0
+    for i in range(len(my_list)):
+        if len(my_list[i]) != 0:
+            counter +=1
+
+
+
+    return counter
