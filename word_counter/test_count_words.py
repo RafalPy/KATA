fe20344b6157cdf40359d9ca6926ac723b44626d
PC <rafalpawezowski@gmail.com>
2025-01-12 19:34:20 +0100

    tests for not counting spaces, tab and new line character added

--- word_counter/test_count_words.py
+++ word_counter/test_count_words.py
@@ -1,5 +1,7 @@
 import unittest
-from kata_word_counter1 import count_words
+
+
+from kata_word_counter import count_words
 
 class TestCountWords(unittest.TestCase):
     def test_regular_delimiter_usage(self):
@@ -10,6 +12,16 @@ class TestCountWords(unittest.TestCase):
         self.assertEqual(count_words("test;test;;;;test", ";"), 3)
     def test_different_delimiter(self):
         self.assertEqual(count_words("test;test;test", "."),1, "The test has failed terribly")
+    def test_single_space_word_not_counted(self):
+        self.assertEqual(count_words("test;test; ;", ";"), 2)
+    def test_multiple_spaces_word_not_counted(self):
+        self.assertEqual(count_words("test.test.    .test", "."), 3)
+    def test_tab_word_not_counted(self):
+        self.assertEqual(count_words("test,test,   ,test", ","), 3)
+    def test_word_with_space_counted(self):
+        self.assertEqual(count_words("test;test; test;test", ";"), 4)
+    def test_new_line_character_not_counted(self):
+        self.assertEqual(count_words("test;test;\n;test", ";"), 3)
 
 if __name__ == '__main__':
     unittest.main()
